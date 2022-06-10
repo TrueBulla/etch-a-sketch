8aece6f467d4f7653f5e8b03104e28ba54a9ae48
matt <matt@matts-MacBook-Air.local>
2022-06-10 18:07:57 -0400

    added more comments to js file

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ const mainContainer = document.querySelector(".main-container")
 const gridContainer = document.querySelector(".grid-container")
 const gridTotalHeight = gridContainer.clientHeight;
 const gridTotalWidth = gridContainer.clientWidth;
-
+//main function 
 function createGrid(){
+    //need to clear the grid of any previous grids before we begin
     clearGrid();
+    //collect a number n to make a square grid of n tiles
     const userPrompt = prompt("How many squares do you want? You must select a number less than 100");
+    //if inputted number is greater than 100, restart the function
     if(userPrompt > 100){
         return createGrid();
     }
+    //creating varibles to collect width of tiles so the number of tiles fit in the alloted space
     let gridSquareHeight = gridTotalHeight / userPrompt;
     let gridSquareWidth = gridTotalWidth / userPrompt;
+    //loop through and create the correct number of rows/columns
     for(let row=0; row<userPrompt; row++){
         const rowDiv = document.createElement("div");
         rowDiv.className = "row";
@@ -23,6 +28,7 @@ function createGrid(){
             colsDiv.style.height = `${gridSquareHeight}px`;
         }
     }
+    //call function to add event listeners to each tile that was created
     addGridListeners();
 }
 createGrid();
@@ -40,16 +46,21 @@ function addGridListeners(){
 }
 //This function will reset the grid to all white spaces
 function clearGrid(){
+    //create a nodelist of all grid tiles
     const gridDivs = document.querySelectorAll(".cols");
+    //takes each tile and removes its background color 
     gridDivs.forEach(gridDiv => {
         gridDiv.style.background = "none";
     })
 }
 //This function removes the grid and then offers to recreate the grid with a different number of rows/columns
 function resetGrid(){
+    //create a nodelist of all grid tile rows
     const rowDivs = document.querySelectorAll('.row')
+    //removes each row of tiles 
     rowDivs.forEach(rowDiv => {
         gridContainer.removeChild(rowDiv)
     })
+    //calls the createGrid function to recreate a grid after the previous one is removed.
     createGrid();
 }
